140c2f7903ae3c372ace4d7b364660456c6b5774
Chris Hjorth <contact@chrishjorth.com>
2020-09-29 11:12:57 +0300

    Updated npm.

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,6 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin()
+    new webpack.HotModuleReplacementPlugin()
   ]
 }
